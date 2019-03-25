86989fe081fcc848b810ca4e3769e893b28cd6b1
zhiyiZeng <1694609389@qq.com>
2019-03-25 15:57:01 +0800

    增加chapter6的代码和数据和模型

--- chapter6/scripts/learn.py
+++ chapter6/scripts/learn.py
@@ -7,7 +7,7 @@ path = r'E:\pythonProjects\cluebearpython\chapter6\data'
 origin_file = pd.read_excel(os.path.join(path, 'shops_nm.xlsx'), encoding='utf8')
 origin_file.head()
 
-shop_names = np.array(list(origin_file['店名'])).reshape((len(shop_names), 1))
+shop_names = np.array(list(origin_file['店名'])).reshape((len(origin_file), 1))
 features = np.random.binomial(1, 0.5, size=len(shop_names) * 5).reshape((len(shop_names), 5))
 shops = np.concatenate((shop_names, features), axis=1)
 
@@ -27,15 +27,76 @@ def judge(x):
 df['y'] = df.apply(judge, axis=1)
 del df['sum']
 df.head()
+df.to_excel(os.path.join(path, 'shops_nm_cleaned.xlsx'), encoding='utf8', index=False)
 
+
+import os
+import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(
-...     X, y, test_size=0.33, random_state=42)
-# TODO: 还有测试集没有切分
 
-X = [[0, 0], [1, 1]]
-Y = [0, 1]
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, Y)
-clf.predict([[2., 2.]])
+# 第一步：数据准备
+data = pd.read_excel(os.path.join(path, 'shops_nm_cleaned.xlsx'), encoding='utf8')
+data.head()
+X_train, X_val, y_train, y_val = train_test_split(data.iloc[:, 1:-1], data.iloc[:, -1], test_size=0.3, random_state=0)
+X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=0.3, random_state=0)
+print('训练集的自变量维度: {}, 因变量维度: {}\n'.format(X_train.shape, y_train.shape))
+print('验证集的自变量维度: {}, 因变量维度: {}\n'.format(X_val.shape, y_val.shape))
+print('测试集的自变量维度: {}, 因变量维度: {}\n'.format(X_test.shape, y_test.shape))
+
+# 第三步：模型训练
+max_depths = [depth for depth in range(2, 10)]
+accuracy = []
+for max_depth in max_depths:
+    clf = tree.DecisionTreeClassifier(class_weight="balanced", max_depth=max_depth)
+    clf = clf.fit(X_train, y_train)
+    acc = clf.score(X_val, y_val)
+    importance_ = clf.feature_importances_
+    accuracy.append([max_depth, acc, importance_])
+
+accuracy
+
+# 第四步：模型评估
+from sklearn.metrics import recall_score, precision_score, accuracy_score
+
+y_val_predict = clf.predict(X_val)
+
+accuracy_score(y_val, y_val_predict)
+recall_score(y_val, y_val_predict)
+precision_score(y_val, y_val_predict)
+
+
+# 第五步：模型调参
+max_depths = [depth for depth in range(2, 10)]
+accuracy = []
+for max_depth in max_depths:
+    clf = tree.DecisionTreeClassifier(class_weight="balanced", max_depth=max_depth)
+    clf = clf.fit(X_train, y_train)
+    acc = clf.score(X_val, y_val)
+    importance_ = clf.feature_importances_
+    accuracy.append([max_depth, acc, importance_])
+
+accuracy
+
+# 第六步：决策过程可视化
+import graphviz
+dot_data = tree.export_graphviz(clf, out_file=None)
+graph = graphviz.Source(dot_data)
+graph.render(os.path.join(model_path, 'decision'))
+graph
+
+
+# 第七步：模型导出和加载
+import os
+from sklearn.externals import joblib
+
+model_path = r'E:\pythonProjects\cluebearpython\chapter6\model'
+files = os.listdir(model_path)
+if 'clf.pkl' not in files:
+    f = open(os.path.join(model_path, 'clf.pkl'), 'wb+')
+    f.close()
+
+joblib.dump(clf, os.path.join(model_path, 'clf.pkl'))
+
+load_clf = joblib.load(os.path.join(model_path, 'clf.pkl'))
+load_clf.score(X_test, y_test)
