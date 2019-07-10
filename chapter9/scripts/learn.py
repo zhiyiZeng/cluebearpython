47d93c42fe48503d77cbd4a6f1006423e397b1c3
zhiyiZeng <1694609389@qq.com>
2019-07-10 15:48:42 +0800

    修改第九章

--- chapter9/scripts/learn.py
+++ chapter9/scripts/learn.py
@@ -4,120 +4,125 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, ForeignKey
 
-
-db_file = r'E:\pythonProjects\cluebearpython\chapter11\data'
+# 改成你存放数据库文件的路径，注意data.db需要提前创建
+db_file = r'E:\pythonProjects\cluebearpython\chapter9\data'
 engine = create_engine('sqlite:///{}'.format(os.path.join(db_file, 'data.db')), encoding='utf8')
 DBSession = sessionmaker(bind=engine)
+# 创建数据库会话实例
 sess = DBSession()
 
 
 Base = declarative_base()
-class Student(Base):
+class ShopBasic(Base):
     # 表的名字:
-    __tablename__ = 'student'
+    __tablename__ = 'basic'
 
     # 表的结构:
     id = Column(Integer, primary_key=True, autoincrement=True)
-    stu_id = Column(Integer, nullable=True)
-    name = Column(String(20))
-    age = Column(Integer)
-    enroll = Column(Integer, nullable=True)
+    name = Column(String(50))
+    time = Column(String(20), nullable=True)
 
 
-class Course(Base):
+class ShopCoupon(Base):
     # 表的名字:
-    __tablename__ = 'course'
+    __tablename__ = 'coupon'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     # 添加外键
-    stu_id = Column(Integer, ForeignKey('student.stu_id'))
+    shop_id = Column(Integer, ForeignKey('basic.id'))
     day = Column(String(5))
-    course = Column(String(10))
+    coupon = Column(String(30))
 
+# 会自动检查表是否存在，如果表不存在，则创建；如果已经存在，则忽略，也可以手动注释，增强可读性。
 Base.metadata.create_all(engine)
 
-students = [
-    (2014114153, '张三', 17, 2018),
-    (2014114154, '李四', 18, 2017),
-    (2014114155, '王五', 16, 2016),
+
+shops = [
+    (311, "老北京涮羊肉", '11:00-21:00', [['周一', '满60减10'], ['周二', '满100减20']]),
+    (312, "大龙燚火锅", '10:00-22:00', [['周一', '满60减10'], ['周二', '满100减20']]),
+    (313, "一尊皇牛", '00:00-24:00', [['周一', '满80减10'], ['周二', '满100减10']]),
 ]
 
-
 news = []
-for stu in students:
-    new = Student(stu_id=stu[0], name=stu[1], age=stu[2], enroll=stu[3])
+for shop in shops:
+    new = ShopBasic(id=shop[0], name=shop[1], time=shop[2])
     news.append(new)
 
 sess.add_all(news)
 sess.commit()
 
-students = sess.query(Student).all()
-for stu in students:
-    print(stu.stu_id, stu.name, stu.age, stu.enroll)
+shops = sess.query(ShopBasic).all()
+for shop in shops:
+    print(shop.id, shop.name, shop.time)
 
-students = sess.query(Student).filter_by(name='张三').all()
-students[0].__dict__
-students = sess.query(Student).order_by('enroll').all()
-for stu in students:
-    print(stu.stu_id, stu.name, stu.age, stu.enroll)
+shops = sess.query(ShopBasic).filter_by(name='老北京涮羊肉').all()
+shops[0].__dict__
 
-student = sess.query(Student).filter_by(name='张三').first()
-student.enroll = 2017
+# 排序
+shops = sess.query(ShopBasic).order_by('name').all()
+for shop in shops:
+    print(shop.id, shop.name, shop.time)
+
+# 修改营业时间
+shop = sess.query(ShopBasic).filter_by(name='老北京涮羊肉').first()
+shop.time = "09:00-21:00"
+sess.commit()
+# 修改后，查看数据是否更新
+shop = sess.query(ShopBasic).filter_by(name='老北京涮羊肉').first()
+shop.__dict__
+
+# 把营业时间设置为None，模拟数据缺失
+shop = sess.query(ShopBasic).filter_by(name='老北京涮羊肉').first()
+shop.time = None
 sess.commit()
 
-student = sess.query(Student).filter_by(name='张三').first()
-student.stu_id = None
-student.enroll = None
+# 删除营业时间缺失的数据
+shop = sess.query(ShopBasic).filter_by(time=None).first()
+sess.delete(shop)
 sess.commit()
 
-student = sess.query(Student).filter_by(name='张三').first()
-student.__dict__
-sess.delete(student)
-sess.commit()
-
-students = sess.query(Student).all()
-for stu in students:
-    print(stu.name)
+shops = sess.query(ShopBasic).all()
+for shop in shops:
+    print(shop.name)
 
 
-
-infos = [
-    (2014114153, '张三', 17, 2018, [['礼拜一', '高等数学'], ['礼拜二', '大学英语']]),
-    (2014114154, '李四', 18, 2017, [['礼拜一', '大学计算机'], ['礼拜二', '大学物理']]),
-    (2014114155, '王五', 16, 2016, [['礼拜一', '国学经典'], ['礼拜二', '创业学']]),
+shops = [
+    (311, "老北京涮羊肉", '11:00-21:00', [['周一', '满60减10'], ['周二', '满100减20']]),
+    (312, "大龙燚火锅", '10:00-22:00', [['周一', '满60减10'], ['周二', '满100减20']]),
+    (313, "一尊皇牛", '00:00-24:00', [['周一', '满80减10'], ['周二', '满100减10']]),
 ]
 
-students = sess.query(Student).all()
-for info in infos:
-    stu_id = info[0]
-    stu = sess.query(Student).filter_by(stu_id=stu_id).first()
-    courses = info[4]
-    course_ls = []
-    for c in courses:
+for shop in shops:
+    shop_id = shop[0]
+    shop_ = sess.query(ShopBasic).filter_by(id=shop_id).first()
+    coupons = shop[3]
+    coupon_ls = []
+    for c in coupons:
         day = c[0]
-        course = c[1]
-        print(day, course)
-        new = Course(
-            stu_id=stu.stu_id,
+        coupon = c[1]
+        print(day, coupon)
+        new = ShopCoupon(
+            shop_id=shop_.id,
             day=day,
-            course=course,
+            coupon=coupon,
         )
-        course_ls.append(new)
+        coupon_ls.append(new)
 
-    sess.add_all(course_ls)
+    sess.add_all(coupon_ls)
     sess.commit()
-courses = sess.query(Course).all()
-for course in courses:
-    print(course.stu_id)
+
+coupons = sess.query(ShopCoupon).all()
+for coupon in coupons:
+    print(coupon.shop_id)
 
 
 import os
 import pandas as pd
 from sqlalchemy import create_engine
 
-db_file = r'E:\pythonProjects\cluebearpython\chapter11\data'
+db_file = r'E:\pythonProjects\cluebearpython\chapter9\data'
 engine = create_engine('sqlite:///{}'.format(os.path.join(db_file, 'data.db')), encoding='utf8')
-df = pd.read_sql('student', engine)
+df = pd.read_sql('basic', engine)
 df
 
 
